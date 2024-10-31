269d7c08f7c26d25fef4d1bda36f099d85bbc4c3
amnamine <amineipad98@gmail0com>
2024-10-31 23:35:38 +0100

    Initial commit of Todo List app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="bg-gradient-to-r from-blue-400 to-purple-600 min-h-screen flex flex-col items-center justify-center p-5">
+    <h1 class="text-4xl font-bold text-white mb-5">Todo List</h1>
+    <div class="todo-container flex mb-4">
+      <input
+        type="text"
+        v-model="newTodo"
+        @keyup.enter="addTodo"
+        placeholder="Add a new task"
+        class="todo-input w-3/4 p-2 rounded-l-lg border border-gray-300 focus:outline-none focus:border-blue-500 transition"
+      />
+      <button @click="addTodo" class="add-button bg-blue-500 text-white rounded-r-lg px-4 hover:bg-blue-600 transition">Add</button>
+    </div>
+    <transition-group name="fade" tag="ul" class="w-full max-w-md">
+      <li
+        v-for="(todo, index) in todos"
+        :key="todo.id"
+        class="todo-item bg-white shadow-md rounded-lg flex items-center justify-between p-3 mb-2 transition-transform transform hover:scale-105"
+      >
+        <div class="flex items-center">
+          <input
+            type="checkbox"
+            v-model="todo.completed"
+            class="checkbox mr-3"
+          />
+          <span :class="{ 'line-through text-gray-400': todo.completed, 'text-gray-800': !todo.completed }" class="text-lg">{{ todo.text }}</span>
+        </div>
+        <button @click="removeTodo(index)" class="remove-button bg-red-500 text-white rounded-lg px-3 hover:bg-red-600 transition">Remove</button>
+      </li>
+    </transition-group>
+    <p v-if="todos.length === 0" class="no-todos text-white">No tasks yet!</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      newTodo: '',
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() === '') return;
+      this.todos.push({ id: Date.now(), text: this.newTodo, completed: false });
+      this.newTodo = '';
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* Animation */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles.css'; // Add this line to include Tailwind CSS
 
 createApp(App).mount('#app')
