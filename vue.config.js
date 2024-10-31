3937cfba36d34004ddd6293eb0ceb42653da5fcf
amnamine <amineipad98@gmail0com>
2024-10-31 23:55:44 +0100

    Update config for GitHub Pages deployment

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ToDoList/' // Update this to your repository name
+    : '/'
+}
